c1f062118c2a3fbc9578be88c02e02cae452bb43
Camilo Mahecha <Camahes@Camilos-MacBook-Pro.local>
2018-10-16 22:05:25 -0500

    Adding Css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,14 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn to design and code React apps.</h1>
-    <p>Lerning how to create a React app from Design+Code. Very intersting stuff here, and a lot to learn.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps.</h1>
+        <p>Lerning how to create a React app from Design+Code. Very intersting stuff here, and a lot to learn.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div> 
+    </div>
+
   </Layout>
 )
 
